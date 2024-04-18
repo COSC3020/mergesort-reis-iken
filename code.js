8be2105fd57ea6f5628032b56b4220673088f3a7
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-18 16:24:07 -0600

    code.js

--- code.js
+++ code.js
@@ -1,17 +1,17 @@
 function mergesort(array) {
-    while (array.length() > 1) {
+    while (array.length > 1) {
         split(array); }
     return array;
 }
 
 function split(array) {
-    let left = array.splice(0, array.length()/2);
+    let left = array.splice(0, array.length/2);
     return merge(split(left), split(array));
 }
 
 function merge(left, right) {
     let array = [];
-    while (left.length() > 0 && right.length() > 0) {
+    while (left.length > 0 && right.length > 0) {
         if (left[0] > right[0] {
             array.push(left[0]); }
         else {
