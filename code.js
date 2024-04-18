7d5b1118ee7210ca17ea863120d8a186e11c2167
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-18 16:41:12 -0600

    code.js

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 function mergesort(array) {
-    let length = arr.length;
-    let temp = new Array(len);
+    let length = array.length;
+    let temp = new Array(length);
 
     for (let i = 1; i < length; i *= 2) {
-        for (let leftbegin = 0; leftbegin < len; leftbegin += 2 * i) {
+        for (let leftbegin = 0; leftbegin < length; leftbegin += 2 * i) {
             let middle = Math.min(leftbegin + i, length);
             const rightbegin = Math.min(leftbegin + 2 * i, length);
             merge(array, temp, leftbegin, middle, rightbegin);
