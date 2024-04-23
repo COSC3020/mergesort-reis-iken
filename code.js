6258f8eeb88922e192683c6f155200ca1eea5d70
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-23 14:08:37 -0600

    code.js

--- code.js
+++ code.js
@@ -1,10 +1,10 @@
 function mergesort(array) {
     let length = array.length;
-    for (let i = 1; i * 2 < length; i *= 2) {
-        for (let leftbegin = 0; leftbegin < length - i; leftbegin += 2 * i) {
-            let middle = leftbegin + i;
+    for (let i = 1; i < length; i *= 2) {
+        for (let leftbegin = 0; leftbegin < length; leftbegin += 2 * i) {
+            let middle = Math.min(leftbegin + i, length);
             const rightbegin = Math.min(leftbegin + 2 * i, length);
-            sort(array, leftbegin, middle, rightbegin);
+            inplaceMerge(array, leftbegin, middle, rightbegin);
         }
     }
     return array;
@@ -13,18 +13,18 @@ function mergesort(array) {
 function sort(array, leftbegin, middle, rightbegin) {
     let left = leftbegin;
     let right = middle;
-    let index = leftbegin;
     while (left < middle && right < rightbegin) {
         if (array[left] <= array[right]) {
-            array[index++] = array[left++];
+            left++;
         } else {
-            array[index++] = array[right++];
+            const temp = array[right];
+            for (let i = right; i > left; i--) {
+                array[i] = array[i - 1];
+            }
+            array[left] = temp;
+            left++;
+            middle++;
+            right++;
         }
-    }
-    while (left < middle) {
-        array[index++] = array[left++];
-    }
-    while (right < rightbegin) {
-        array[index++] = array[right++];
     }
 }
