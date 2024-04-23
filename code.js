d6adbb7215e1205cfcb706b4f9cc236ab61939fe
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-23 14:09:11 -0600

    code.js

--- code.js
+++ code.js
@@ -3,8 +3,8 @@ function mergesort(array) {
     for (let i = 1; i < length; i *= 2) {
         for (let leftbegin = 0; leftbegin < length; leftbegin += 2 * i) {
             let middle = Math.min(leftbegin + i, length);
-            const rightbegin = Math.min(leftbegin + 2 * i, length);
-            inplaceMerge(array, leftbegin, middle, rightbegin);
+            let rightbegin = Math.min(leftbegin + 2 * i, length);
+            sort(array, leftbegin, middle, rightbegin);
         }
     }
     return array;
@@ -17,7 +17,7 @@ function sort(array, leftbegin, middle, rightbegin) {
         if (array[left] <= array[right]) {
             left++;
         } else {
-            const temp = array[right];
+            let temp = array[right];
             for (let i = right; i > left; i--) {
                 array[i] = array[i - 1];
             }
